993bb3f81f05a9cfaf93dafe6182005ad8818dd2
shuvokhan3 <shuvoj360@gmail.com>
2025-04-19 23:38:52 +0600

    work with customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,115 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    //customer page view
+    public function viewCustomer(){
+        return view('pages.dashboard.customer-page');
+    }
+
+    //create customer
+    public function CustomerStore(Request $request){
+        try{
+            $user_id = $request->header('id');
+            Customer::create([
+                'name'=>$request->input('name'),
+                'email'=>$request->input('email'),
+                'mobile'=>$request->input('mobile'),
+                'user_id'=>$user_id
+            ]);
+            return response()->json([
+                'status'=>'success',
+                'message'=>'Customer Added Successfully'
+            ]);
+        }catch (\Exception $e){
+            return response()->json([
+                'status'=>'error',
+                'message'=>'Customer Create Error'
+            ]);
+        }
+    }
+
+    //customers display
+    public function CustomerList(Request $request){
+        try{
+            $user_id = $request->header('id');
+            return Customer::where('user_id','=',$user_id)->get();
+        }catch (\Exception $e){
+            return response()->json([
+                'status'=>'error',
+                'message'=>'Customer List Error'
+            ]);
+        }
+    }
+
+    //Customers Delete
+    public function CustomerDelete(Request $request){
+        try{
+            $customer_id = $request->input('id');
+            $user_id = $request->header('id');
+            Customer::where('user_id', '=', $user_id)
+                ->where('id', '=', $customer_id)
+                ->delete();
+            return response()->json([
+                'status'=>'success',
+                'message'=>'Customer Delete Successfully'
+            ]);
+        }catch (\Exception $e){
+            return response()->json([
+                'status'=>'Error',
+                'message'=>'Customer Delete Error'
+            ]);
+        }
+    }
+
+    //customer By id
+    public function CustomerById(Request $request){
+        try{
+            $customer_id = $request->input('id');
+            $user_id = $request->header('id');
+            Customer::where('user_id', '=',$user_id)
+                ->where('id', '=' ,$customer_id)->first();
+
+            return response()->json([
+                'status'=>'success',
+                'message'=>'Customer Detail Found'
+            ]);
+        }catch (\Exception $e){
+            return response()->json([
+                'status'=>'error',
+                'message'=>'Customer Select Error'
+            ]);
+
+        }
+    }
+
+    //customer update
+    public function CustomerUpdate(Request $request){
+        try{
+            $customer_id = $request->input('id');
+            $user_id = $request->header('id');
+            Customer::where('user_id', '=', $user_id)
+                ->where('id', '=', $customer_id)
+                ->update([
+                    'name'=> $request->input('name'),
+                    'email'=>$request->input('email'),
+                    'mobile'=>$request->input('mobile')
+                ]);
+            return response()->json([
+                'status'=>'success',
+                'message'=>'Customer Update Successfully'
+            ]);
+        }catch (\Exception $e){
+            return response()->json([
+                'status'=>'error',
+                'message'=> $e->getMessage()
+            ]);
+        }
+
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\TokenVerificationMiddleware;
 use Illuminate\Support\Facades\Route;
 
-//web api route
+
 //registration route
 Route::post('/userRegistration',[UserController::class,'UserRegistration']);
 //login route
 Route::post('/userLogin',[UserController::class,'UserLogin']);
+//user logout
+Route::get('/logout', [UserController::class, 'UserLogout'])->middleware(TokenVerificationMiddleware::class);
 //otp route
 Route::post('/sendOtpCode',[UserController::class,'SendOtpCode']);
 Route::post('/verifyOTP',[UserController::class,'verifyOTP']);
@@ -20,9 +23,11 @@ Route::post('/passwordReset',[UserController::class,'PasswordReset'])
 Route::get('/getUserProfileData',[UserController::class, 'getUserProfileData'])->middleware(TokenVerificationMiddleware::class);
 Route::post('/upDateUserProfileData',[UserController::class, 'upDateUserProfileData'])->middleware(TokenVerificationMiddleware::class);
 
-//user logout
-Route::get('/logout', [UserController::class, 'UserLogout'])->middleware(TokenVerificationMiddleware::class);
 
+//category route
+
+//category page route
+Route::get('/categoryPage',[CategoryController::class,'categoryView'])->middleware(TokenVerificationMiddleware::class);
 //category backend route
 //category create
 Route::post('/createCategory',[CategoryController::class,'CategoryStore'])->middleware(TokenVerificationMiddleware::class);
@@ -35,6 +40,17 @@ Route::get('/ShowCategoryUpdateFormData',[CategoryController::class, 'ShowCatego
 //update Category
 Route::post('/categoryUpdate',[CategoryController::class,'CategoryUpdate'])->middleware(TokenVerificationMiddleware::class);
 
+//customer route
+//customer page route
+Route::get('/customerPage',[CustomerController::class,'viewCustomer'])->middleware(TokenVerificationMiddleware::class);
+//customer backend route
+//customer create
+Route::post('/createCustomer',[CustomerController::class,'CustomerStore'])->middleware(TokenVerificationMiddleware::class);
+Route::get('/customerList',[CustomerController::class, 'CustomerList'])->middleware(TokenVerificationMiddleware::class);
+Route::post('/customerDelete',[CustomerController::class,'CustomerDelete'])->middleware(TokenVerificationMiddleware::class);
+Route::get('/customerById',[CustomerController::class, 'CustomerById'])->middleware(TokenVerificationMiddleware::class);
+Route::post('/customerUpdate',[CustomerController::class,'CustomerUpdate'])->middleware(TokenVerificationMiddleware::class);
+
 
 //page route
 Route::get('/login',[UserController::class,'ViewLogin']);
@@ -44,6 +60,3 @@ Route::get('/sendOtp',[UserController::class,'SendOTP']);
 Route::get('/verifyOTP',[UserController::class, 'vOTP']);
 Route::get('/dashboard',[UserController::class,'Dashboard'])->middleware(TokenVerificationMiddleware::class);
 Route::get('/userProfile',[UserController::class, 'userProfile'])->middleware(TokenVerificationMiddleware::class);
-
-//category page route
-Route::get('/categoryPage',[CategoryController::class,'categoryView'])->middleware(TokenVerificationMiddleware::class);
