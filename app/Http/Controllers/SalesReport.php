daacdcf38d281f19d93b50813f7f98ae7625e4a7
shuvokhan3 <shuvoj360@gmail.com>
2025-04-29 15:32:45 +0600

    Sales Report Generator Backend

---
+++ app/Http/Controllers/SalesReport.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Invoice;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\In;
+
+class SalesReport extends Controller
+{
+    public function SalesReport()
+    {
+        return view('pages.dashboard.sale-report-page');
+    }
+
+    public function SalesReportData(Request $request){
+
+        try{
+
+
+            $user_id=$request->header('id');
+            $FormDate=date('Y-m-d',strtotime($request->FormDate));
+            $ToDate=date('Y-m-d',strtotime($request->ToDate));
+
+        $total=Invoice::where('user_id',$user_id)
+            ->whereDate('created_at', '>=', $FormDate)
+            ->whereDate('created_at', '<=', $ToDate)
+            ->sum('total');
+
+
+        $vat=Invoice::where('user_id',$user_id)
+            ->whereDate('created_at', '>=', $FormDate)
+            ->whereDate('created_at', '<=', $ToDate)
+            ->sum('vat');
+        $payable=Invoice::where('user_id',$user_id)
+            ->whereDate('created_at', '>=', $FormDate)
+            ->whereDate('created_at', '<=', $ToDate)
+            ->sum('payable');
+        $discount=Invoice::where('user_id',$user_id)
+            ->whereDate('created_at', '>=', $FormDate)
+            ->whereDate('created_at', '<=', $ToDate)
+            ->sum('discount');
+
+
+
+        $list=Invoice::where('user_id',$user_id)
+            ->whereDate('created_at', '>=', $FormDate)
+            ->whereDate('created_at', '<=', $ToDate)
+            ->with('customer')
+            ->get();
+
+
+        $data=[
+            'payable'=> $payable,
+            'discount'=>$discount,
+            'total'=> $total,
+            'vat'=> $vat,
+            'list'=>$list,
+            'FormDate'=>$request->FormDate,
+            'ToDate'=>$request->FormDate
+        ];
+
+
+
+        //install Dompdf pakages
+
+        $pdf = Pdf::loadView('report.SalesReport',$data);
+
+        return $pdf->download('SaleReport.pdf');
+
+
+
+        }catch (\Exception $exception){
+            return response()->json([
+                'status' => 'Error',
+                'message' => $exception->getMessage()
+            ]);
+        }
+
+    }
+
+
+
+
+}
