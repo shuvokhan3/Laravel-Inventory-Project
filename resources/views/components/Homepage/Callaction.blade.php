241b861bc3a3c482412f5a9ef7a6dfbbb0ecbb44
shuvokhan3 <shuvoj360@gmail.com>
2025-05-08 15:03:07 +0600

    Call to action component

---
+++ resources/views/components/Homepage/Callaction.blade.php
@@ -0,0 +1,25 @@
+<!-- Call action section -->
+<section
+    id="call-action"
+    class="section-area !bg-primary !text-primary-color"
+>
+    <div class="container">
+        <div class="scroll-revealed text-center max-w-[550px] mx-auto">
+            <h2 class="mb-8 text-inherit">
+                We love to make perfect solutions for your business
+            </h2>
+            <p>
+                Why I say old chap that is, spiffing off his nut cor blimey
+                guvnords geeza bloke knees up bobby, sloshed arse William cack
+                Richard. Bloke fanny around chesed of bum bag old lost the pilot
+                say there spiffing off his nut.
+            </p>
+            <a
+                href="javascript:void(0)"
+                class="inline-block px-5 py-3 rounded-md mt-11 bg-green-400 text-white hover:bg-green-500 hover:text-white focus:bg-green-500 focus:text-white"
+                role="button"
+            >Get Started Now</a
+            >
+        </div>
+    </div>
+</section>
