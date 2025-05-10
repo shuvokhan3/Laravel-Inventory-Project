34fc359fef131603924382425945b971c653a073
shuvokhan3 <shuvoj360@gmail.com>
2025-05-10 22:36:17 +0600

    Pricing components

---
+++ resources/views/components/Homepage/Pricing.blade.php
@@ -0,0 +1,240 @@
+
+<!-- Pricing section -->
+<section id="pricing" class="section-area">
+    <div class="container mt-6">
+        <div class="scroll-revealed text-center max-w-[550px] mx-auto mb-12">
+            <h6 class="mb-2 block text-lg font-semibold text-primary">
+                Pricing
+            </h6>
+            <h2 class="mb-6">Pricing & Plans</h2>
+            <p>
+                There are many variations of passages of Lorem Ipsum available but
+                the majority have suffered alteration in some form.
+            </p>
+        </div>
+
+        <div class="row">
+            <div class="scroll-revealed col-12 sm:col-6 lg:col-4">
+                <div
+                    class="rounded-xl py-12 px-9 bg-body-light-1 dark:bg-body-dark-12/10 text-center shadow-card-1 hover:shadow-lg"
+                >
+                    <div>
+                        <h6
+                            class="inline-block font-medium text-base mb-6 text-primary bg-primary/10 rounded-md py-2 px-8 pricing-title"
+                        >
+                            Starter
+                        </h6>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            Iste, similique ea.
+                        </p>
+                        <div class="pt-8">
+                            <h2
+                                class="font-semibold inline-block relative pl-4 text-[55px]"
+                            >
+                      <span
+                          class="font-normal text-body-light-11 dark:text-body-dark-11 text-[1.25rem] absolute left-0 top-1.5"
+                      >$</span
+                      >0<span
+                                    class="text-[1.125rem] inline-block text-body-light-11 dark:text-body-dark-11 font-normal"
+                                >/mo</span
+                                >
+                            </h2>
+                        </div>
+                    </div>
+                    <div class="pt-8 pb-10">
+                        <a
+                            href="javascript:void(0)"
+                            class="inline-block font-medium px-6 py-3 rounded-md bg-primary/10 text-primary hover:bg-primary hover:text-primary-color focus:bg-primary focus:text-primary-color"
+                        >Start Free Trial</a
+                        >
+                    </div>
+                    <div>
+                        <ul>
+                            <li
+                                class="text-left relative mb-3 inline-flex gap-3 w-full"
+                            >
+                                <i
+                                    class="lni lni-checkmark-circle text-primary text-base leading-[24px]"
+                                ></i>
+                                <span>Cras justo odio.</span>
+                            </li>
+                            <li
+                                class="text-left relative mb-3 inline-flex gap-3 w-full"
+                            >
+                                <i
+                                    class="lni lni-checkmark-circle text-primary text-base leading-[24px]"
+                                ></i>
+                                <span>Dapibus ac facilisis in.</span>
+                            </li>
+                            <li
+                                class="text-left relative mb-3 inline-flex gap-3 w-full"
+                            >
+                                <i
+                                    class="lni lni-checkmark-circle text-body-light-11 dark:text-body-dark-11 text-base leading-[24px]"
+                                ></i>
+                                <span>Morbi leo risus.</span>
+                            </li>
+                            <li
+                                class="text-left relative mb-3 inline-flex gap-3 w-full"
+                            >
+                                <i
+                                    class="lni lni-checkmark-circle text-body-light-11 dark:text-body-dark-11 text-base leading-[24px]"
+                                ></i>
+                                <span>Excepteur sint occaecat velit.</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="scroll-revealed col-12 sm:col-6 lg:col-4">
+                <div
+                    class="rounded-xl py-12 px-9 bg-body-light-1 dark:bg-body-dark-12/10 text-center shadow-lg"
+                >
+                    <div>
+                        <h6
+                            class="inline-block font-medium text-base mb-6 text-primary-color bg-primary rounded-md py-2 px-8 pricing-title"
+                        >
+                            Exclusive
+                        </h6>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            Iste, similique ea.
+                        </p>
+                        <div class="pt-8">
+                            <h2
+                                class="font-semibold inline-block relative pl-4 text-[55px]"
+                            >
+                      <span
+                          class="font-normal text-body-light-11 dark:text-body-dark-11 text-[1.25rem] absolute left-0 top-1.5"
+                      >$</span
+                      >99<span
+                                    class="text-[1.125rem] inline-block text-body-light-11 dark:text-body-dark-11 font-normal"
+                                >/mo</span
+                                >
+                            </h2>
+                        </div>
+                    </div>
+                    <div class="pt-8 pb-10">
+                        <a
+                            href="javascript:void(0)"
+                            class="inline-block font-medium px-6 py-3 rounded-md bg-primary text-primary-color hover:bg-primary-light-10 dark:hover:bg-primary-dark-10 hover:text-primary-color focus:bg-primary-light-10 dark:focus:bg-primary-dark-10 focus:text-primary-color"
+                        >Purchase Now</a
+                        >
+                    </div>
+                    <div>
+                        <ul>
+                            <li
+                                class="text-left relative mb-3 inline-flex gap-3 w-full"
+                            >
+                                <i
+                                    class="lni lni-checkmark-circle text-primary text-base leading-[24px]"
+                                ></i>
+                                <span>Cras justo odio.</span>
+                            </li>
+                            <li
+                                class="text-left relative mb-3 inline-flex gap-3 w-full"
+                            >
+                                <i
+                                    class="lni lni-checkmark-circle text-primary text-base leading-[24px]"
+                                ></i>
+                                <span>Dapibus ac facilisis in.</span>
+                            </li>
+                            <li
+                                class="text-left relative mb-3 inline-flex gap-3 w-full"
+                            >
+                                <i
+                                    class="lni lni-checkmark-circle text-primary text-base leading-[24px]"
+                                ></i>
+                                <span>Morbi leo risus.</span>
+                            </li>
+                            <li
+                                class="text-left relative mb-3 inline-flex gap-3 w-full"
+                            >
+                                <i
+                                    class="lni lni-checkmark-circle text-body-light-11 dark:text-body-dark-11 text-base leading-[24px]"
+                                ></i>
+                                <span>Excepteur sint occaecat velit.</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="scroll-revealed col-12 sm:col-6 lg:col-4">
+                <div
+                    class="rounded-xl py-12 px-9 bg-body-light-1 dark:bg-body-dark-12/10 text-center shadow-card-1 hover:shadow-lg"
+                >
+                    <div>
+                        <h6
+                            class="inline-block font-medium text-base mb-6 text-primary bg-primary/10 rounded-md py-2 px-8 pricing-title"
+                        >
+                            Premium
+                        </h6>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            Iste, similique ea.
+                        </p>
+                        <div class="pt-8">
+                            <h2
+                                class="font-semibold inline-block relative pl-4 text-[55px]"
+                            >
+                      <span
+                          class="font-normal text-body-light-11 dark:text-body-dark-11 text-[1.25rem] absolute left-0 top-1.5"
+                      >$</span
+                      >150<span
+                                    class="text-[1.125rem] inline-block text-body-light-11 dark:text-body-dark-11 font-normal"
+                                >/mo</span
+                                >
+                            </h2>
+                        </div>
+                    </div>
+                    <div class="pt-8 pb-10">
+                        <a
+                            href="javascript:void(0)"
+                            class="inline-block font-medium px-6 py-3 rounded-md bg-primary/10 text-primary hover:bg-primary hover:text-primary-color focus:bg-primary focus:text-primary-color"
+                        >Purchase Now</a
+                        >
+                    </div>
+                    <div>
+                        <ul>
+                            <li
+                                class="text-left relative mb-3 inline-flex gap-3 w-full"
+                            >
+                                <i
+                                    class="lni lni-checkmark-circle text-primary text-base leading-[24px]"
+                                ></i>
+                                <span>Cras justo odio.</span>
+                            </li>
+                            <li
+                                class="text-left relative mb-3 inline-flex gap-3 w-full"
+                            >
+                                <i
+                                    class="lni lni-checkmark-circle text-primary text-base leading-[24px]"
+                                ></i>
+                                <span>Dapibus ac facilisis in.</span>
+                            </li>
+                            <li
+                                class="text-left relative mb-3 inline-flex gap-3 w-full"
+                            >
+                                <i
+                                    class="lni lni-checkmark-circle text-primary text-base leading-[24px]"
+                                ></i>
+                                <span>Morbi leo risus.</span>
+                            </li>
+                            <li
+                                class="text-left relative mb-3 inline-flex gap-3 w-full"
+                            >
+                                <i
+                                    class="lni lni-checkmark-circle text-primary text-base leading-[24px]"
+                                ></i>
+                                <span>Excepteur sint occaecat velit.</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
