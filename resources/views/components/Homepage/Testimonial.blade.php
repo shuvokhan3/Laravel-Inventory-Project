03feaea96e3840254afab2610ab1d18d333b89ab
shuvokhan3 <shuvoj360@gmail.com>
2025-05-10 22:37:10 +0600

    Testimonial component

---
+++ resources/views/components/Homepage/Testimonial.blade.php
@@ -0,0 +1,166 @@
+<!-- Testimonials section -->
+<section id="testimonials" class="section-area">
+    <div class="container">
+        <div class="scroll-revealed text-center max-w-[550px] mx-auto mb-12">
+            <h6 class="mb-2 block text-lg font-semibold text-primary">
+                Client Reviews
+            </h6>
+            <h2 class="mb-6">Our Testimonials</h2>
+            <p>
+                There are many variations of passages of Lorem Ipsum available but
+                the majority have suffered alteration in some form.
+            </p>
+        </div>
+
+        <div
+            class="swiper testimonial-carousel common-carousel scroll-revealed"
+        >
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div
+                        class="rounded-xl bg-body-light-1 dark:bg-body-dark-12/10 px-5 py-8 shadow-card-2 sm:px-8"
+                    >
+                        <p
+                            class="mb-6 text-base text-body-light-11 dark:text-body-dark-11"
+                        >
+                            "Our members are so impressed. It's intuitive. It's clean.
+                            It's distraction free. If you're building a community."
+                        </p>
+                        <figure class="flex items-center gap-4">
+                            <div class="h-[50px] w-[50px] overflow-hidden">
+                                <img
+                                    src="{{asset('images/img/avatar/avatar-4.jpg')}}"
+                                    alt="Testimonial picture"
+                                    class="h-full w-full rounded-full object-cover"
+                                />
+                            </div>
+                            <figcaption class="flex-grow">
+                                <h3
+                                    class="text-sm font-semibold text-body-light-11 dark:text-body-dark-11"
+                                >
+                                    Ralf Nacht
+                                </h3>
+                                <p
+                                    class="text-xs text-body-light-10 dark:text-body-dark-10"
+                                >
+                                    Web Entrepreneur
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+
+                <div class="swiper-slide">
+                    <div
+                        class="rounded-xl bg-body-light-1 dark:bg-body-dark-12/10 px-5 py-8 shadow-card-2 sm:px-8"
+                    >
+                        <p
+                            class="mb-6 text-base text-body-light-11 dark:text-body-dark-11"
+                        >
+                            "Our members are so impressed. It's intuitive. It's clean.
+                            It's distraction free. If you're building a community."
+                        </p>
+                        <figure class="flex items-center gap-4">
+                            <div class="h-[50px] w-[50px] overflow-hidden">
+                                <img
+                                    src="{{asset('images/img/avatar/avatar-5.jpg')}}"
+                                    alt="Testimonial picture"
+                                    class="h-full w-full rounded-full object-cover"
+                                />
+                            </div>
+                            <figcaption class="flex-grow">
+                                <h3
+                                    class="text-sm font-semibold text-body-light-11 dark:text-body-dark-11"
+                                >
+                                    Sebastian Toft
+                                </h3>
+                                <p
+                                    class="text-xs text-body-light-10 dark:text-body-dark-10"
+                                >
+                                    Web Developer
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+
+                <div class="swiper-slide">
+                    <div
+                        class="rounded-xl bg-body-light-1 dark:bg-body-dark-12/10 px-5 py-8 shadow-card-2 sm:px-8"
+                    >
+                        <p
+                            class="mb-6 text-base text-body-light-11 dark:text-body-dark-11"
+                        >
+                            "Our members are so impressed. It's intuitive. It's clean.
+                            It's distraction free. If you're building a community."
+                        </p>
+                        <figure class="flex items-center gap-4">
+                            <div class="h-[50px] w-[50px] overflow-hidden">
+                                <img
+                                    src="{{asset('images/img/avatar/avatar-6.jpg')}}"
+                                    alt="Testimonial picture"
+                                    class="h-full w-full rounded-full object-cover"
+                                />
+                            </div>
+                            <figcaption class="flex-grow">
+                                <h3
+                                    class="text-sm font-semibold text-body-light-11 dark:text-body-dark-11"
+                                >
+                                    Bao Shen
+                                </h3>
+                                <p
+                                    class="text-xs text-body-light-10 dark:text-body-dark-10"
+                                >
+                                    UI/UX Designer
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+
+                <div class="swiper-slide">
+                    <div
+                        class="rounded-xl bg-body-light-1 dark:bg-body-dark-12/10 px-5 py-8 shadow-card-2 sm:px-8"
+                    >
+                        <p
+                            class="mb-6 text-base text-body-light-11 dark:text-body-dark-11"
+                        >
+                            "Our members are so impressed. It's intuitive. It's clean.
+                            It's distraction free. If you're building a community."
+                        </p>
+                        <figure class="flex items-center gap-4">
+                            <div class="h-[50px] w-[50px] overflow-hidden">
+                                <img
+                                    src="{{asset('images/img/avatar/avatar-7.jpg')}}"
+                                    alt="Testimonial picture"
+                                    class="h-full w-full rounded-full object-cover"
+                                />
+                            </div>
+                            <figcaption class="flex-grow">
+                                <h3
+                                    class="text-sm font-semibold text-body-light-11 dark:text-body-dark-11"
+                                >
+                                    Emanuel Velzeboer
+                                </h3>
+                                <p
+                                    class="text-xs text-body-light-10 dark:text-body-dark-10"
+                                >
+                                    Graphics Designer
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mt-[60px] flex items-center justify-center gap-1">
+                <div class="swiper-button-prev">
+                    <i class="lni lni-arrow-left"></i>
+                </div>
+                <div class="swiper-button-next">
+                    <i class="lni lni-arrow-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
